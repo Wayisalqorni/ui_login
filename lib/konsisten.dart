cc59e46fe8e14372e53598d4185e5fad4535c116
wayisalqorni <wayisalqornits@gmail.com>
2023-05-19 09:03:18 +0700

    first commit

---
+++ lib/konsisten.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color whiteColor = Color(0xffffffffff);
+Color textColor = Color(0xff0D0140);
+Color secondaryTextColor = Color(0xff524B6B);
+Color buttonColor = Color(0xffE6E1FF);
+Color primaryButtonColor = Color(0xff130160);
+Color tncButtonColor = Color(0xff130160);
+Color greyColor = Colors.grey;
+
+TextStyle whiteTextStyle = GoogleFonts.dmSans(
+  color: whiteColor,
+);
+
+TextStyle textTextStyle = GoogleFonts.dmSans(
+  color: textColor,
+);
+
+TextStyle secondaryTextStyle = GoogleFonts.dmSans(
+  color: secondaryTextColor,
+);
+
+TextStyle tncTextStyle = GoogleFonts.dmSans(
+  color: tncButtonColor,
+);
+TextStyle greyTextStyle = GoogleFonts.dmSans(
+  color: greyColor,
+);
+FontWeight bold = FontWeight.bold;
